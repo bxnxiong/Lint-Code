1088f3dcce84cc5bad6de5099a2f924f337917bf
bxnxiong <bxnxiong@gmail.com>
2016-10-09 13:51:55 -0400

    version containing duplicate numbers(eg.999111)

---
+++ 160 findMinimumInRotatedArray-duplicateNumbers.py
@@ -0,0 +1,35 @@
+class Solution:
+    # @param nums: a rotated sorted array
+    # @return: the minimum number in the array
+    def findMin(self, nums):
+        # write your code here
+        
+        n = len(nums)
+        if nums[0] >= nums[-1] and n > 1:
+            if n == 0:return
+            if n == 2 or n == 3:
+                i = 0
+                while i+1 < n:
+                    if nums[i] > nums[i+1]:
+                        return nums[i+1]
+                    i += 1
+                return
+            left = nums[:n/2]
+            right = nums[n/2:]
+            if len(set(left)) > 1 and len(set(right)) > 1:
+                if left[0] > left[-1]:
+                    return self.findMin(left)
+                else:
+                    return self.findMin(right)
+            elif len(set(left)) > 1:
+                return self.findMin(left)
+            elif len(set(right)) > 1:
+                return self.findMin(right)
+            else:
+                if left[0] > right[0]:
+                    return right[0]
+                else:
+                    return left[0]
+        else:
+            return nums[0]
+            
