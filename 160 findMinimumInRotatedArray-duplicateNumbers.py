b62d6abd4ce6edcccff7606d2b1f432b8370b5f5
bxnxiong <bxnxiong@gmail.com>
2016-10-09 15:00:34 -0400

    optimized logical expression

--- 160 findMinimumInRotatedArray-duplicateNumbers.py
+++ 160 findMinimumInRotatedArray-duplicateNumbers.py
@@ -21,15 +21,17 @@ class Solution:
                     return self.findMin(left)
                 else:
                     return self.findMin(right)
-            elif len(set(left)) > 1:
-                return self.findMin(left)
-            elif len(set(right)) > 1:
-                return self.findMin(right)
-            else:
-                if left[0] > right[0]:
+            elif len(set(right)) == 1:
+                if left[0] > left[-1] or left[0] <= right[0]:
+                    return self.findMin(left)
+                else:
                     return right[0]
+            else:
+                if right[0] > right[1] or right[0] <= left[0]:
+                    return self.findMin(right)
                 else:
                     return left[0]
+                
         else:
             return nums[0]
             
