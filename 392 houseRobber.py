5e8dc041e78ec8488f83dfdd0a655f4fa1948d40
bxnxiong <bxnxiong@gmail.com>
2016-10-28 15:15:55 -0400

    simple dynamic programming problem

---
+++ 392 houseRobber.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param A: a list of non-negative integers.
+    # return: an integer
+    def houseRobber(self, A):
+        # write your code here
+        if A == []: return 0
+        if len(A) <= 2: return max(A)
+        
+        max_sum = [0 for i in range(len(A))]
+        
+        max_sum[0] = A[0]
+        max_sum[1] = A[1]
+        
+        for i in range(2,len(A)):
+            max_sum[i] = max(max_sum[i-1],max_sum[i-2]+A[i])
+        return max_sum[-1]
